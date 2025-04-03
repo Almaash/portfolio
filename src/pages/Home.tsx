a116dfe1d3f3a51c25dfcd1b0df0e0fe7217f25c
Almaash <alamalmaash@gmail.com>
2025-04-03 18:16:14 +0530

    sxcv

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer/Footer";
-import Header from "../components/Home/Header";
+// import Header from "../components/Home/Header";
 import HeaderNew from "../components/Home/HeaderNew";
 // import Header from "../components/Home/Header";
 // import HeaderNew from "../components/Home/Header2";
