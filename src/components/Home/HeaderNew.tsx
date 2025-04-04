215c2e472c8617bed87e0d4f7d8a7f8b3bf77011
Almaash <alamalmaash@gmail.com>
2025-04-04 11:32:49 +0530

    asdf

--- src/components/Home/HeaderNew.tsx
+++ src/components/Home/HeaderNew.tsx
@@ -2,6 +2,7 @@ import { Globe } from "lucide-react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import headerVideo from "../../assets/videos/newww.mp4";
+import headerImage from "../../assets/videos/image.png";
 
 const HeaderNew = () => {
   useGSAP(() => {
@@ -27,9 +28,17 @@ const HeaderNew = () => {
   return (
     <div className="relative flex justify-center items-center min-h-screen bg-[#0D1117] overflow-hidden">
       {/* Video Background */}
-      <video className="absolute inset-0 w-full h-full object-cover" src={headerVideo} autoPlay loop muted  playsInline></video>
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src={headerVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster={headerImage}
+      ></video>
       <div className="absolute inset-0 bg-black/70"></div>
-      
+
       {/* Content */}
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-4 md:px-6 ">
         <div className="bg-white/10 backdrop-blur-2xl p-6 md:p-10 rounded-2xl md:rounded-3xl shadow-2xl border border-white/30 text-center w-full max-w-[90%] sm:max-w-[75%] md:max-w-[60%] lg:max-w-[50%] ">
@@ -40,7 +49,7 @@ const HeaderNew = () => {
             Full Stack Developer
           </p>
           <div className="mt-3 flex items-center justify-center gap-2 sm:gap-3 text-base sm:text-lg text-gray-400">
-            <Globe className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7" /> 
+            <Globe className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7" />
             <span className="tracking-widest font-semibold">INDIA</span>
           </div>
           <p className="mt-2 text-xs sm:text-sm text-gray-400">est. 2024</p>
